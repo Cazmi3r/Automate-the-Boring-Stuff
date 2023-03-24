8766b95968e5303d430d3990cd12f5060fe20595
Andrew Paulikas <apaulikas@govisiondirect.com>
2023-03-24 14:12:34 -0400

    can now search a longer starting

--- Automate the Boring Stuff/regex/isPhoneNumber.py
+++ Automate the Boring Stuff/regex/isPhoneNumber.py
@@ -16,7 +16,9 @@ def isPhoneNumber(text):
              return False
     return True
 
-print('Is 415-555-4242 a phone number?')
-print(isPhoneNumber('415-555-4242'))
-print('Is Moshi moshi a phone number?')
-print(isPhoneNumber('Moshi moshi'))
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+   chunk = message[i:i+12]
+   if isPhoneNumber(chunk):
+          print('Phone number found: ' + chunk)
+print('Done')
