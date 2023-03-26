b5525de6cf496a7c211e5a6b2afec5786e7fb714
Raxath <36684140+Raxath@users.noreply.github.com>
2023-03-26 17:52:48 -0400

    function requires this regex to be formated

--- Automate the Boring Stuff/regex/phoneAndEMail.py
+++ Automate the Boring Stuff/regex/phoneAndEMail.py
@@ -4,12 +4,12 @@
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?                # area code
-    (\s|-|\.)?                        # separator
-    (\d{3})                           # first 3 digits
+    (\d{3}|\(\d{3}\))?                # area code [1]
+    (\s|-|\.)?                        # separator 
+    (\d{3})                           # first 3 digits [3]
     (\s|-|\.)                         # separator
-    (\d{4})                           # last 4 digits
-    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    (\d{4})                           # last 4 digits 
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension 
     )''', re.VERBOSE)
 
 emailRegex = re.compile(r'''(
