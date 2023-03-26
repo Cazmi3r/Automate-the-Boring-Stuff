644119f75d581ff37e9fa651032c9b252e6d6f64
Raxath <36684140+Raxath@users.noreply.github.com>
2023-03-26 15:19:48 -0400

    base file from project

---
+++ Automate the Boring Stuff/regex/phoneAndEMail.py
@@ -0,0 +1,19 @@
+#! python3
+# phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
+
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
+
+# TODO: Create email regex.
+
+# TODO: Find matches in clipboard text.
+
+# TODO: Copy results to the clipboard.
