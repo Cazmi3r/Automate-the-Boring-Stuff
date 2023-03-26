87f72a2a9fd3b0efdf3fe4299ddde49eac77897e
Raxath <36684140+Raxath@users.noreply.github.com>
2023-03-26 15:20:23 -0400

    code smell

--- Automate the Boring Stuff/regex/phoneAndEMail.py
+++ Automate the Boring Stuff/regex/phoneAndEMail.py
@@ -5,9 +5,9 @@ import pyperclip, re
 
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?                # area code
-    (\s|-|\.)?                        # separator
+    [ .-]?                        # separator
     (\d{3})                           # first 3 digits
-    (\s|-|\.)                         # separator
+    [ .-]                         # separator
     (\d{4})                           # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
     )''', re.VERBOSE)
